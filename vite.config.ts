0b9cdf45cb20ec9b459ef06688f22e1542ced4fd
tkt <37575408+tktcorporation@users.noreply.github.com>
2024-11-08 16:52:40 +0000

    feat: pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+import { remixPWA } from '@remix-pwa/dev'
 
 export default defineConfig({
-  plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
+  plugins: [remix(), netlifyPlugin(), tsconfigPaths(), remixPWA()],
 });
